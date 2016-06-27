1d55b5abf5db29cccae765a5c35fde6aa9c6ce4d
maj03169 <jan.mayer.91@icloud.com>
2016-06-27 11:02:24 +0200

    Added infos.txt took me whole fucking day :(

---
+++ app/pages/festivalInfo/festivalInfo.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by jan on 27.06.16.
+ */
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+    templateUrl: 'build/pages/festivalInfo/festivalInfo.html'
+})
+export class FestivalInfoPage {
+    constructor(private navController: NavController) {
+
+    }
+    
+}
