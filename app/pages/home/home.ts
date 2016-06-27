f2cb12395ca6d2fd1eb97a8653cb5fcc1e2dfab4
maj03169 <jan.mayer.91@icloud.com>
2016-06-27 11:02:24 +0200

    Added FestivalInfoPage

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {FestivalInfoPage} from "../festivalInfo/festivalInfo";
+
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -7,5 +9,11 @@ import {NavController} from 'ionic-angular';
 export class HomePage {
   constructor(private navController: NavController) {
   
+  }
+
+  openFestivalInfo(festivalInfo) {
+    this.navController.push(FestivalInfoPage, {
+      festivalInfo: festivalInfo
+    })
   }
 }
