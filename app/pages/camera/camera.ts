389c71f78efe4a04aa5d94aad2c656acbb96ce2a
maj03169 <jan.mayer.91@icloud.com>
2016-07-10 21:00:51 +0200

    Made camera page responsive

--- app/pages/camera/camera.ts
+++ app/pages/camera/camera.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-import {FestivalInfoPage} from "../festivalInfo/festivalInfo";
 
 @Component({
   templateUrl: 'build/pages/camera/camera.html'
